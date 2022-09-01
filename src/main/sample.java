1e8adc42cb985e45c354bd8fb8f4a84ccbefc715
sreekanth-07 <112549794+sreekanth-07@users.noreply.github.com>
2022-09-01 10:52:36 +0530

    Create sample.java

---
+++ src/main/sample.java
@@ -0,0 +1,34 @@
+import org.apache.commons.httpclient.Cookie;
+import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+ 
+public class GetCookiePrintAndSetValue {
+ 
+  public static void main(String args[]) throws Exception {
+ 
+    HttpClient client = new HttpClient();
+    client.getParams().setParameter("j_username", "abc");
+    client.getParams().setParameter("j_password", "pqr");
+ 
+    GetMethod method = new GetMethod("http://localhost:8080/");
+    try{
+      client.executeMethod(method);
+      Cookie[] cookies = client.getState().getCookies();
+      for (int i = 0; i < cookies.length; i++) {
+        Cookie cookie = cookies[i];
+        System.err.println(
+          "Cookie: " + cookie.getName() +
+          ", Value: " + cookie.getValue() +
+          ", IsPersistent?: " + cookie.isPersistent() +
+          ", Expiry Date: " + cookie.getExpiryDate() +
+          ", Comment: " + cookie.getComment());
+        }
+      client.executeMethod(method);
+    } catch(Exception e) {
+      System.err.println(e);
+    } finally {
+      method.releaseConnection();
+    }
+  }
+}
