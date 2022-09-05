fce358abc1c53b4dd1d5ee4e84efdac4cd50e656
sreekanth-07 <112549794+sreekanth-07@users.noreply.github.com>
2022-09-05 15:47:18 +0530

    Update pmd.bat

--- bin/pmd.bat
+++ bin/pmd.bat
@@ -1,6 +1,6 @@
-@echo off
+@echo on
 set TOPDIR="%~dp0.."
 set OPTS=
 set MAIN_CLASS=net.sourceforge.pmd.PMD
-
+echo %PMD_JAVA_OPTS%
 java %PMD_JAVA_OPTS% -classpath %TOPDIR%\lib\* %OPTS% %MAIN_CLASS% %*
