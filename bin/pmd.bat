7cae99dd98b371b1fcc182e42b116b7e60187645
sreekanth-07 <112549794+sreekanth-07@users.noreply.github.com>
2022-09-07 11:04:08 +0530

    Delete bin directory

--- bin/pmd.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-set TOPDIR="%~dp0.."
-set OPTS=
-set MAIN_CLASS=net.sourceforge.pmd.PMD
-echo %PMD_JAVA_OPTS%
-echo "sreekanth"
-java %PMD_JAVA_OPTS% -classpath %TOPDIR%\lib\* %OPTS% %MAIN_CLASS% %*
